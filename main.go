2ee04c98a05c7eae97c822373489038d9e87f545
anmoljain16 <ajanmoldosi@gmail.com>
2023-02-02 02:13:46 +0530

    Adding mux and starting server at 8000

--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
+
 type Movie struct {
 	Id       string    `json:"id"`
 	Isbn     string    `json:"isbn"`
@@ -10,4 +17,23 @@ type Movie struct {
 type Director struct {
 	Firstname string `json:"firstname"`
 	Lastname  string `json:"lastname"`
+}
+
+var movies []Movie
+
+func main() {
+
+	r := mux.NewRouter()
+
+	movies = append(movies, Movie{Id: "1", Isbn: "696969", Title: "Ace ki Maut", Director: &Director{Firstname: "oda", Lastname: "Loda"}})
+	movies = append(movies, Movie{Id: "2", Isbn: "565656", Title: "Luffy ki Maut", Director: &Director{Firstname: "saif", Lastname: "chumitya"}})
+
+	r.HandleFunc("/movies", getMovies).Methods("GET")
+	r.HandleFunc("/movies/{id}", getMovies).Methods("GET")
+	r.HandleFunc("/movies", createMovie).Methods("POST")
+	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
+	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
+
+	fmt.Println("Starting server...")
+	log.Fatal(http.ListenAndServe(":8000", r))
 }
